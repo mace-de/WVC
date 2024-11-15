83ee414bf990dd5ebe65a40aa3aeecdd127299fc
mace-de <op101@gmx.de>
2024-11-15 23:37:57 +0100

    151124-23

--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,8 @@ const uint8_t sinus1[] = {0, 6, 13, 19, 25, 31, 37, 44, 50, 56, 62, 68, 74, 80, 
                           44, 37, 31, 25, 19, 13, 6,
                           0, 0, 0};
 
-const uint32_t minimalspannung_abs = 410 * Mittel_aus;  //= etwa 26V darunter kann der Wandler wegen zu geringer Ausgangsspannung an den Scheitelpunkten nicht ins Netz einspeisen
-const uint32_t maximalstrom_abs = 700 * Mittel_aus / 6; //= etwa 15A darüber wird der WR wohl verglühen
+const uint32_t minimalspannung_abs = 1576 * Mittel_aus; //= etwa 26V darunter kann der Wandler wegen zu geringer Ausgangsspannung an den Scheitelpunkten nicht ins Netz einspeisen
+const uint32_t maximalstrom_abs = 2200 * Mittel_aus;    //= etwa 15A darüber wird der WR wohl verglühen
 
 volatile uint32_t Synccounter = 0, abregelwert = 0, cnt100ms = 0, gu, gi;
 volatile uint8_t U_out = 0, counter = 0;
@@ -49,10 +49,10 @@ void tsk_main(void *param)
 {
   while (1)
   {
-    static uint8_t Schritt = 1, cnt_a = 0, startverz = 0;
-    static bool enable, teilbereichsuche = false;
+    volatile static uint8_t Schritt = 1, cnt_a = 0, startverz = 0;
+    volatile static bool enable, teilbereichsuche = false;
     volatile static uint32_t spannung_MPP = 0, Langzeitzaehler = 0, minimalspannung = 0, maximalspannung = 0, maximalstrom = 0;
-    static uint32_t leistung_MPP = 0;
+    volatile static uint32_t leistung_MPP = 0;
     volatile static uint32_t spannung = 0, strom = 0, spannung_a[Mittel_aus], strom_a[Mittel_aus], temperatur = 0;
     static uint32_t leistung;
     // sobald die Reglerfreigabe weg geht oder die Netzsyncronität verloren geht Wandler Stom abschalten und in den Warteschritt gehen
@@ -68,14 +68,14 @@ void tsk_main(void *param)
       if (cnt_a > Mittel_aus - 1)
         cnt_a = 0;
       spannung -= spannung_a[cnt_a];
-      spannung_a[cnt_a] = adc_channel_sample(ADC_CHANNEL_7); // 1V=0,05V am ADC 3,3v=66v
+      spannung_a[cnt_a] = adc_channel_sample(ADC_CHANNEL_7); // 970 (61) = 1V
       spannung += spannung_a[cnt_a];
       strom -= strom_a[cnt_a];
-      strom_a[cnt_a] = adc_channel_sample(ADC_CHANNEL_1)-130; // 1A=0,14V am ADC 3,3v=23,6a
+      strom_a[cnt_a] = adc_channel_sample(ADC_CHANNEL_1) - 130; // 2375 (148) = 1A
       strom += strom_a[cnt_a];
       cnt_a++;
-      gu=spannung;
-      gi=strom;
+      gu = spannung;
+      gi = strom;
       if (Schritt == 2)
       { // im Suchschritt suchen wir hier den Punkt mit der grösten Leistung und merken uns die Spannung dort. Auf die regeln wir später.
         leistung = spannung * strom;
@@ -90,7 +90,7 @@ void tsk_main(void *param)
     else
     {
       flag100ms = false;
-      temperatur = adc_channel_sample(ADC_CHANNEL_8);                        // Temperatur im Gehäuse messen
+      temperatur = adc_channel_sample(ADC_CHANNEL_8);      // Temperatur im Gehäuse messen
       maximalstrom = maximalstrom_abs * (6 - abregelwert); // aktuellen Maximalstrom aus maximalem Wechselrichterstrom und dem Abregelwert berechnen
       switch (Schritt)
       {
@@ -99,8 +99,12 @@ void tsk_main(void *param)
         {
           if (!enable && Sync)
           {
-            if (startverz < 50) // wenn startklar noch 5 Sekunden warten
+            if (startverz < 50)
+            { // wenn startklar noch 5 Sekunden warten
               startverz++;
+              gpio_bit_reset(GPIOB, GPIO_PIN_12);
+              gpio_bit_write(GPIOB, GPIO_PIN_13, (FlagStatus)!gpio_output_bit_get(GPIOB, GPIO_PIN_13));
+            }
             else
             {
               minimalspannung = minimalspannung_abs;
@@ -113,13 +117,19 @@ void tsk_main(void *param)
             }
           }
           else
+          {
+            gpio_bit_reset(GPIOB, GPIO_PIN_12);
+            gpio_bit_set(GPIOB, GPIO_PIN_13);
             startverz = 0;
+          }
           break;
         }
       case 2: // MPP finden Schritt
         // Wandlerstom kontinuirlich hoch fahren bis die Zellspannung auf die Minimalspannung sinkt oder der Strom auf den Maximalstrom steigt
         // dabei den Punkt mit der grösten Leistung suchen und sich die Spannung dort merken = MPP Spannung
         {
+          gpio_bit_reset(GPIOB, GPIO_PIN_13);
+          gpio_bit_write(GPIOB, GPIO_PIN_12, (FlagStatus)!gpio_output_bit_get(GPIOB, GPIO_PIN_12));
           if (maximalspannung < spannung)
             maximalspannung = spannung;
           if (teilbereichsuche)
@@ -151,6 +161,8 @@ void tsk_main(void *param)
         }
       case 3: // Regelbetrieb Schritt
       {
+        gpio_bit_set(GPIOB, GPIO_PIN_12);
+        gpio_bit_reset(GPIOB, GPIO_PIN_13);
         // bei plötzlicher Verschattung leistung schnell reduzieren (Spannung fällt unter 24V)
         if (spannung < (minimalspannung_abs - 38 * Mittel_aus))
         {
@@ -200,12 +212,14 @@ void tsk_com(void *param)
 {
   while (1)
   {
-    Serial.print("T13 RR: ");              /// 260.147);
-    Serial.println(TIMER_CAR(TIMER13)); /// 260.147);
-    Serial.print("V: ");              /// 260.147);
-    Serial.println(gu); /// 260.147);
-    Serial.print("A: ");              /// 260.147);
-    Serial.println(gi); /// 260.147);
+    Serial.print("Hz: ");           /// 260.147);
+    Serial.println(TIMER_CAR(TIMER13)/112.5); /// 260.147);
+    Serial.print("V: ");                /// 260.147);
+    Serial.println(gu/970.0);                 /// 260.147);
+    Serial.print("A: ");                /// 260.147);
+    Serial.println(gi/2375.0);                 /// 260.147);
+    Serial.print("W: ");                /// 260.147);
+    Serial.println((gu*gi)/2678779.07);                 /// 260.147);
 
     vTaskDelay(1000);
   }
@@ -215,16 +229,16 @@ HardwareTimer mytim(TIMER13);
 void zcd_int()
 {
   if (Synccounter < 256)
-    TIMER_CAR(TIMER13)--; // Timer 1 so nachführen dass er netzsyncron läuft
+    TIMER_CAR(TIMER13)
+    --; // Timer 1 so nachführen dass er netzsyncron läuft
   if (Synccounter > 256)
-    TIMER_CAR(TIMER13)++; // müsste bei 50Hz bei OCR1C=156 sein
+    TIMER_CAR(TIMER13)
+    ++; // müsste bei 50Hz bei OCR1C=156 sein
   if (Synccounter == 256)
   {
     Sync = 1;
-    digitalWrite(PB12, 1);
-    digitalWrite(PB13, 0);
     digitalWrite(PC13, 1);
-    digitalWrite(PA12, 0);    
+    digitalWrite(PA12, 0);
   }
   if ((TIMER_CAR(TIMER13) < 5500) || (TIMER_CAR(TIMER13) > 5800))
     Sync = 0; // Netzfrequenz überwachen Grenze 52,06Hz und 48,82Hz
@@ -248,10 +262,8 @@ void t13ov()
   if (Synccounter > 258)
   {
     Sync = 0;
-    digitalWrite(PB12, 0);
-    digitalWrite(PB13, 1);
     digitalWrite(PC13, 0);
-    digitalWrite(PA12, 1);    
+    digitalWrite(PA12, 1);
   }
 }
 void setup()
@@ -264,31 +276,31 @@ void setup()
   TIMER_CH0CV(TIMER15) = 1;
   mypwm.start();
   mytim.setPrescaler(1);
-  mytim.setReloadValue(5625);
+  mytim.setReloadValue(5625); //112,5 / 1Hz
   mytim.attachInterrupt(t13ov, 0);
   mytim.start();
   Serial.begin(9600);
-  pinMode(PB13, OUTPUT);//LED-rot
-  pinMode(PB12, OUTPUT);//LED-blau
-  pinMode(PC13, OUTPUT);//Relais-Netz
-  pinMode(PA12, OUTPUT);//Enable PWM-Regler
-  pinMode(PB11, OUTPUT);//Relais 115V/230V
+  pinMode(PB13, OUTPUT); // LED-rot
+  pinMode(PB12, OUTPUT); // LED-blau
+  pinMode(PC13, OUTPUT); // Relais-Netz
+  pinMode(PA12, OUTPUT); // Enable PWM-Regler
+  pinMode(PB11, OUTPUT); // Relais 115V/230V
   digitalWrite(PB11, 1);
   digitalWrite(PA12, 1);
   digitalWrite(PB12, 0);
   digitalWrite(PB13, 1);
   digitalWrite(PC13, 0);
-  pinMode(PA7,INPUT_ANALOG);//PV-Spannung
-  pinMode(PA1,INPUT_ANALOG);//PV-Strom
-  pinMode(PB0,INPUT_ANALOG);//Temperatursensor
-  pinMode(PA0,INPUT_ANALOG);//Spannung Netz (glatt)
-  pinMode(PA4,INPUT_ANALOG);//Spannung Netz (puls)
-  pinMode(PA6, INPUT);//Zerocross
+  pinMode(PA7, INPUT_ANALOG); // PV-Spannung
+  pinMode(PA1, INPUT_ANALOG); // PV-Strom
+  pinMode(PB0, INPUT_ANALOG); // Temperatursensor
+  pinMode(PA0, INPUT_ANALOG); // Spannung Netz (glatt)
+  pinMode(PA4, INPUT_ANALOG); // Spannung Netz (puls)
+  pinMode(PA6, INPUT);        // Zerocross
 
   adc_config();
   xTaskCreate(tsk_main, "task1", 200, NULL, 0, &hdl1);
   xTaskCreate(tsk_com, "task2", 100, NULL, 1, &hdl2);
-  //queue1 = xQueueCreate(1, 8);
+  // queue1 = xQueueCreate(1, 8);
   attachInterrupt(PA6, zcd_int, RISING);
   vTaskStartScheduler();
 }
@@ -298,21 +310,21 @@ void loop()
 }
 void adc_config(void)
 {
-    rcu_periph_clock_enable(RCU_ADC);
-    rcu_adc_clock_config(RCU_ADCCK_APB2_DIV4);
-    /* ADC data alignment config */
-    adc_data_alignment_config(ADC_DATAALIGN_RIGHT);
-    /* ADC channel length config */
-    adc_channel_length_config(ADC_REGULAR_CHANNEL, 1U);
-    /* ADC trigger config */
-    adc_external_trigger_source_config(ADC_REGULAR_CHANNEL, ADC_EXTTRIG_REGULAR_NONE);
-    /* ADC external trigger config */
-    adc_external_trigger_config(ADC_REGULAR_CHANNEL, ENABLE);
-    /* enable ADC interface */
-    adc_enable();
-    delay(1U);
-    /* ADC calibration and reset calibration */
-    adc_calibration_enable();
+  rcu_periph_clock_enable(RCU_ADC);
+  rcu_adc_clock_config(RCU_ADCCK_APB2_DIV4);
+  /* ADC data alignment config */
+  adc_data_alignment_config(ADC_DATAALIGN_RIGHT);
+  /* ADC channel length config */
+  adc_channel_length_config(ADC_REGULAR_CHANNEL, 1U);
+  /* ADC trigger config */
+  adc_external_trigger_source_config(ADC_REGULAR_CHANNEL, ADC_EXTTRIG_REGULAR_NONE);
+  /* ADC external trigger config */
+  adc_external_trigger_config(ADC_REGULAR_CHANNEL, ENABLE);
+  /* enable ADC interface */
+  adc_enable();
+  delay(1U);
+  /* ADC calibration and reset calibration */
+  adc_calibration_enable();
 }
 
 /*!
@@ -323,14 +335,15 @@ void adc_config(void)
 */
 uint16_t adc_channel_sample(uint8_t channel)
 {
-    /* ADC regular channel config */
-    adc_regular_channel_config(0U, channel, ADC_SAMPLETIME_7POINT5);
-    /* ADC software trigger enable */
-    adc_software_trigger_enable(ADC_REGULAR_CHANNEL);
-    /* wait the end of conversion flag */
-    while(!adc_flag_get(ADC_FLAG_EOC));
-    /* clear the end of conversion flag */
-    adc_flag_clear(ADC_FLAG_EOC);
-    /* return regular channel sample value */
-    return (adc_regular_data_read());
+  /* ADC regular channel config */
+  adc_regular_channel_config(0U, channel, ADC_SAMPLETIME_7POINT5);
+  /* ADC software trigger enable */
+  adc_software_trigger_enable(ADC_REGULAR_CHANNEL);
+  /* wait the end of conversion flag */
+  while (!adc_flag_get(ADC_FLAG_EOC))
+    ;
+  /* clear the end of conversion flag */
+  adc_flag_clear(ADC_FLAG_EOC);
+  /* return regular channel sample value */
+  return (adc_regular_data_read());
 }
