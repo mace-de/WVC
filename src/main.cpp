ab62e5dfcec26103100d3fcde4e6e79f65d8d183
mace-de <op101@gmx.de>
2024-11-16 08:57:12 +0100

    161124-09

--- src/main.cpp
+++ src/main.cpp
@@ -27,21 +27,17 @@ const uint8_t sinus1[] = {0, 6, 13, 19, 25, 31, 37, 44, 50, 56, 62, 68, 74, 80, 
                           44, 37, 31, 25, 19, 13, 6,
                           0, 0, 0};
 
-const uint32_t minimalspannung_abs = 1576 * Mittel_aus; //= etwa 26V darunter kann der Wandler wegen zu geringer Ausgangsspannung an den Scheitelpunkten nicht ins Netz einspeisen
-const uint32_t maximalstrom_abs = 2200 * Mittel_aus;    //= etwa 15A darüber wird der WR wohl verglühen
+const uint32_t minimalspannung_abs = 1576 * Mittel_aus;      //= etwa 26V darunter kann der Wandler wegen zu geringer Ausgangsspannung
+                                                             // an den Scheitelpunkten nicht ins Netz einspeisen
+const uint32_t maximalstrom_abs = (2200 * Mittel_aus) / 225; //= etwa 15A darüber wird der WR wohl verglühen
 
-volatile uint32_t Synccounter = 0, abregelwert = 0, cnt100ms = 0, gu, gi;
+volatile uint32_t Synccounter = 0, abregelwert = 0, cnt100ms = 0, gu, gi, gun;
 volatile uint8_t U_out = 0, counter = 0;
 volatile bool flag100ms = 0, Sync = 0;
 
 TaskHandle_t hdl1, hdl2;
 HardwareSerial Serial(PB7, PB6, 0);
-xQueueHandle queue1;
-struct s1
-{
-  uint32_t adcmax;
-  uint32_t adcmin;
-};
+
 void adc_config(void);
 uint16_t adc_channel_sample(uint8_t channel);
 
@@ -49,15 +45,14 @@ void tsk_main(void *param)
 {
   while (1)
   {
-    volatile static uint8_t Schritt = 1, cnt_a = 0, startverz = 0;
-    volatile static bool enable, teilbereichsuche = false;
-    volatile static uint32_t spannung_MPP = 0, Langzeitzaehler = 0, minimalspannung = 0, maximalspannung = 0, maximalstrom = 0;
-    volatile static uint32_t leistung_MPP = 0;
-    volatile static uint32_t spannung = 0, strom = 0, spannung_a[Mittel_aus], strom_a[Mittel_aus], temperatur = 0;
-    static uint32_t leistung;
-    // sobald die Reglerfreigabe weg geht oder die Netzsyncronität verloren geht Wandler Stom abschalten und in den Warteschritt gehen
-    enable = 0;
-    if (enable || !Sync)
+    static uint8_t Schritt = 1, cnt_a = 0;
+    static bool teilbereichsuche = false;
+    static uint32_t spannung_MPP = 0, Langzeitzaehler = 0, minimalspannung = 0, maximalspannung = 0, maximalstrom = 0, startverz = 0;
+    static uint32_t leistung_MPP = 0;
+    static uint32_t spannung = 0, strom = 0, spannung_a[Mittel_aus], strom_a[Mittel_aus], temperatur = 0;
+    uint32_t leistung, netzspannung;
+    // sobald die Netzsyncronität verloren geht Wandler Stom abschalten und in den Warteschritt gehen
+    if (!Sync)
     {
       Schritt = 1;
       U_out = 0;
@@ -74,6 +69,14 @@ void tsk_main(void *param)
       strom_a[cnt_a] = adc_channel_sample(ADC_CHANNEL_1) - 130; // 2375 (148) = 1A
       strom += strom_a[cnt_a];
       cnt_a++;
+      netzspannung = adc_channel_sample(ADC_CHANNEL_0); // 4,186 / 1V
+      if ((netzspannung > 1060) || (netzspannung < 880)) // wenn Netzspannung unter 210V oder über 253V dann Wechselrichter aus 
+      {
+        Sync = 0;
+        gpio_bit_reset(GPIOC, GPIO_PIN_13); // Netzrelais aus
+        gpio_bit_set(GPIOA, GPIO_PIN_12);   // Regler aus
+      }
+      gun= netzspannung;
       gu = spannung;
       gi = strom;
       if (Schritt == 2)
@@ -90,23 +93,25 @@ void tsk_main(void *param)
     else
     {
       flag100ms = false;
-      temperatur = adc_channel_sample(ADC_CHANNEL_8);      // Temperatur im Gehäuse messen
-      maximalstrom = maximalstrom_abs * (6 - abregelwert); // aktuellen Maximalstrom aus maximalem Wechselrichterstrom und dem Abregelwert berechnen
+      temperatur = adc_channel_sample(ADC_CHANNEL_8);        // Temperatur im Gehäuse messen
+      maximalstrom = maximalstrom_abs * (225 - abregelwert); // aktuellen Maximalstrom aus maximalem Wechselrichterstrom und dem Abregelwert berechnen
       switch (Schritt)
       {
       case 1: // Warteschritt
-        // auf die Reglerfreigabe vom Hauptcontroller und Netzsyncronität warten
+        // auf Netzsyncronität warten
         {
-          if (!enable && Sync)
+          if (Sync)
           {
-            if (startverz < 50)
-            { // wenn startklar noch 5 Sekunden warten
+            if (startverz < 300)
+            { // wenn startklar noch 30 Sekunden warten
               startverz++;
-              gpio_bit_reset(GPIOB, GPIO_PIN_12);
-              gpio_bit_write(GPIOB, GPIO_PIN_13, (FlagStatus)!gpio_output_bit_get(GPIOB, GPIO_PIN_13));
+              gpio_bit_reset(GPIOB, GPIO_PIN_12);                                                       // blau aus
+              gpio_bit_write(GPIOB, GPIO_PIN_13, (FlagStatus)!gpio_output_bit_get(GPIOB, GPIO_PIN_13)); // rot blinkt
             }
             else
-            {
+            {                                     // wenn Wartezeit abgelaufen starten
+              gpio_bit_set(GPIOC, GPIO_PIN_13);   // Netzrelais ein
+              gpio_bit_reset(GPIOA, GPIO_PIN_12); // Regler ein
               minimalspannung = minimalspannung_abs;
               maximalspannung = minimalspannung_abs;
               Langzeitzaehler = 0;
@@ -117,9 +122,9 @@ void tsk_main(void *param)
             }
           }
           else
-          {
-            gpio_bit_reset(GPIOB, GPIO_PIN_12);
-            gpio_bit_set(GPIOB, GPIO_PIN_13);
+          {                                     // wenn kein Netz
+            gpio_bit_reset(GPIOB, GPIO_PIN_12); // blau aus
+            gpio_bit_set(GPIOB, GPIO_PIN_13);   // rot leuchtet
             startverz = 0;
           }
           break;
@@ -128,8 +133,8 @@ void tsk_main(void *param)
         // Wandlerstom kontinuirlich hoch fahren bis die Zellspannung auf die Minimalspannung sinkt oder der Strom auf den Maximalstrom steigt
         // dabei den Punkt mit der grösten Leistung suchen und sich die Spannung dort merken = MPP Spannung
         {
-          gpio_bit_reset(GPIOB, GPIO_PIN_13);
-          gpio_bit_write(GPIOB, GPIO_PIN_12, (FlagStatus)!gpio_output_bit_get(GPIOB, GPIO_PIN_12));
+          gpio_bit_reset(GPIOB, GPIO_PIN_13);                                                       // rot aus
+          gpio_bit_write(GPIOB, GPIO_PIN_12, (FlagStatus)!gpio_output_bit_get(GPIOB, GPIO_PIN_12)); // blau blinkt
           if (maximalspannung < spannung)
             maximalspannung = spannung;
           if (teilbereichsuche)
@@ -161,18 +166,19 @@ void tsk_main(void *param)
         }
       case 3: // Regelbetrieb Schritt
       {
-        gpio_bit_set(GPIOB, GPIO_PIN_12);
-        gpio_bit_reset(GPIOB, GPIO_PIN_13);
+        gpio_bit_set(GPIOB, GPIO_PIN_12);   // blau leuchtet
+        gpio_bit_reset(GPIOB, GPIO_PIN_13); // rot aus
         // bei plötzlicher Verschattung leistung schnell reduzieren (Spannung fällt unter 24V)
         if (spannung < (minimalspannung_abs - 38 * Mittel_aus))
         {
           U_out = U_out >> 1;
         }
         // ansonsten Wandlerstrom kontinuirlich so einstellen dass die Zellen mit der in Schritt 2 gefundenen MPP Spannung laufen
+        // und die Temperatur auf der platine nicht über 70°C steigt
         else
         {
-          if ((spannung < spannung_MPP) || (strom > maximalstrom) || (temperatur < 500))
-          { // 72°C
+          if ((spannung < spannung_MPP) || (strom > maximalstrom) || (temperatur < 550))
+          { // 70°C
             if (U_out > 0)
               U_out--; // Ausgangsstrom reduzieren
           }
@@ -207,102 +213,118 @@ void tsk_main(void *param)
     }
   }
 }
-volatile uint32_t val;
+
 void tsk_com(void *param)
 {
   while (1)
   {
-    Serial.print("Hz: ");           /// 260.147);
-    Serial.println(TIMER_CAR(TIMER13)/112.5); /// 260.147);
-    Serial.print("V: ");                /// 260.147);
-    Serial.println(gu/970.0);                 /// 260.147);
-    Serial.print("A: ");                /// 260.147);
-    Serial.println(gi/2375.0);                 /// 260.147);
-    Serial.print("W: ");                /// 260.147);
-    Serial.println((gu*gi)/2678779.07);                 /// 260.147);
-
+    Serial.print("Netzfrequenz: ");
+    Serial.println(TIMER_CAR(TIMER13) / 112.5);
+    Serial.print("Netzspannung: ");
+    Serial.println(gun / 4.186);
+    Serial.print("PV-Spannung: ");
+    Serial.println(gu / 970.0);
+    Serial.print("PV-Strom: ");
+    Serial.println(gi / 2375.0);
+    Serial.print("Einspeiseleistung: ");
+    Serial.println((gu * gi) / 2678779.07);
+    Serial.println();
     vTaskDelay(1000);
   }
 }
-PWM mypwm(PB8);
-HardwareTimer mytim(TIMER13);
+
 void zcd_int()
 {
   if (Synccounter < 256)
     TIMER_CAR(TIMER13)
-    --; // Timer 1 so nachführen dass er netzsyncron läuft
+  --; // Timer 1 so nachführen dass er netzsyncron läuft
   if (Synccounter > 256)
     TIMER_CAR(TIMER13)
-    ++; // müsste bei 50Hz bei OCR1C=156 sein
+  ++; // müsste bei 50Hz bei 5625 sein
   if (Synccounter == 256)
   {
     Sync = 1;
-    digitalWrite(PC13, 1);
-    digitalWrite(PA12, 0);
   }
-  if ((TIMER_CAR(TIMER13) < 5500) || (TIMER_CAR(TIMER13) > 5800))
-    Sync = 0; // Netzfrequenz überwachen Grenze 52,06Hz und 48,82Hz
-  if ((TIMER_CAR(TIMER13) >= 5500) && (TIMER_CAR(TIMER13) <= 5620))
-    abregelwert = 5620 - TIMER_CAR(TIMER13); // Zwischen 50 und 52Hz Leistung abregeln
+  if ((TIMER_CAR(TIMER13) < 5400) || (TIMER_CAR(TIMER13) > 5737)) // Netzfrequenz überwachen Grenze 52,08Hz und 49,02Hz
+  {
+    Sync = 0;
+    gpio_bit_reset(GPIOC, GPIO_PIN_13); // Netzrelais aus
+    gpio_bit_set(GPIOA, GPIO_PIN_12);   // Regler aus
+  }
+  if ((TIMER_CAR(TIMER13) >= 5400) && (TIMER_CAR(TIMER13) <= 5620))
+  {
+    abregelwert = 5625 - TIMER_CAR(TIMER13); // Zwischen 50 und 52Hz Leistung abregeln
+  }
   Synccounter = 0;
 }
-void t13ov()
+
+void t13ov_int()
 {
-  TIMER_CH0CV(TIMER15) = (sinus2[(uint8_t)Synccounter & 0b01111111] * (uint32_t)U_out) >> 8; // 8
-  if (cnt100ms < 1282)
+  TIMER_CH0CV(TIMER15) = (sinus2[(uint8_t)Synccounter & 0b01111111] * (uint32_t)U_out) >> 8;
+  if (cnt100ms < 1282) // alle 1282 x 78us = 100ms (72MHz/5625=78us)
   {
     cnt100ms++;
   }
   else
-  {               // alle 1282 x 78us = 100ms (16MHz/8/156=78us)
+  {
     cnt100ms = 0; // das Flag für die Hauptschleife setzen
     flag100ms = true;
   }
   Synccounter++;
-  if (Synccounter > 258)
+  if (Synccounter > 258) // wenn Synccounter überläuft = kein Zerocross = kein Netz
   {
     Sync = 0;
-    digitalWrite(PC13, 0);
-    digitalWrite(PA12, 1);
+    gpio_bit_reset(GPIOC, GPIO_PIN_13); // Netzrelais aus
+    gpio_bit_set(GPIOA, GPIO_PIN_12);   // Regler aus
   }
 }
+
+PWM mypwm(PB8);
+HardwareTimer mytim(TIMER13);
 void setup()
 {
-  // put your setup code here, to run once:
 
-  mypwm.setPeriodCycle(100, 0, FORMAT_US);
-  TIMER_PSC(TIMER15) = 6;
-  TIMER_CAR(TIMER15) = 1000;
-  TIMER_CH0CV(TIMER15) = 1;
-  mypwm.start();
-  mytim.setPrescaler(1);
-  mytim.setReloadValue(5625); //112,5 / 1Hz
-  mytim.attachInterrupt(t13ov, 0);
-  mytim.start();
   Serial.begin(9600);
-  pinMode(PB13, OUTPUT); // LED-rot
-  pinMode(PB12, OUTPUT); // LED-blau
-  pinMode(PC13, OUTPUT); // Relais-Netz
-  pinMode(PA12, OUTPUT); // Enable PWM-Regler
-  pinMode(PB11, OUTPUT); // Relais 115V/230V
-  digitalWrite(PB11, 1);
-  digitalWrite(PA12, 1);
-  digitalWrite(PB12, 0);
-  digitalWrite(PB13, 1);
-  digitalWrite(PC13, 0);
+
+  // I/O-Pins einstellen
+  pinMode(PB13, OUTPUT);      // LED-rot
+  pinMode(PB12, OUTPUT);      // LED-blau
+  pinMode(PC13, OUTPUT);      // Relais-Netz
+  pinMode(PA12, OUTPUT);      // Enable PWM-Regler (active low!)
+  pinMode(PB11, OUTPUT);      // Relais 115V/230V
   pinMode(PA7, INPUT_ANALOG); // PV-Spannung
   pinMode(PA1, INPUT_ANALOG); // PV-Strom
   pinMode(PB0, INPUT_ANALOG); // Temperatursensor
   pinMode(PA0, INPUT_ANALOG); // Spannung Netz (glatt)
   pinMode(PA4, INPUT_ANALOG); // Spannung Netz (puls)
   pinMode(PA6, INPUT);        // Zerocross
-
+  digitalWrite(PB11, 1);      // Relais 115V/230V immer auf 230V
+  digitalWrite(PA12, 1);      // PWM-Regler aus
+  digitalWrite(PB12, 0);      // LED-blau aus
+  digitalWrite(PB13, 1);      // LED-rot ein
+  digitalWrite(PC13, 0);      // Relais-Netz aus
+  //--------------------------------------------------------------------------
   adc_config();
-  xTaskCreate(tsk_main, "task1", 200, NULL, 0, &hdl1);
-  xTaskCreate(tsk_com, "task2", 100, NULL, 1, &hdl2);
-  // queue1 = xQueueCreate(1, 8);
-  attachInterrupt(PA6, zcd_int, RISING);
-  vTaskStartScheduler();
+
+  // Timer für PWM und Haupttakt einstellen 10kHz PWM und 72MHz/256/5625=50Hz
+  // einstellen über Arduino Funktionen dann Zugriff nur noch über SPL
+
+  mypwm.setPeriodCycle(100, 0);
+  TIMER_PSC(TIMER15) = 6;
+  TIMER_CAR(TIMER15) = 1000;
+  TIMER_CH0CV(TIMER15) = 1;
+  mypwm.start();
+  mytim.setPrescaler(1);
+  mytim.setReloadValue(5625); // 112,5 / 1Hz
+  mytim.attachInterrupt(t13ov_int, 0);
+  mytim.start();
+  //---------------------------------------------------------------------------
+
+  // Tasks und Interrupt erzeugen
+  xTaskCreate(tsk_main, "task1", 200, NULL, 0, &hdl1); // Haupttask
+  xTaskCreate(tsk_com, "task2", 100, NULL, 1, &hdl2);  // Kommunikationstask
+  attachInterrupt(PA6, zcd_int, RISING);               // Zerocross Interrupt
+  vTaskStartScheduler();                               // Tasks starten
 }
 
 void loop()
