b6105d3a845b38c2467afde5bb9a0bb81d5851fa
mace-de <op101@gmx.de>
2024-11-24 09:26:36 +0100

    241124-09

--- include/FlashStorage_mod.h
+++ include/FlashStorage_mod.h
@@ -25,7 +25,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * File modified for small buffer to save SRAM and correct page size
  */
 
@@ -40,66 +40,77 @@ class FlashStorage
     static_assert(_storage_size % 4 == 0,
                   "Storage must be a size multiple of 4!");
 
-    private:
-        uint8_t buffer_[_storage_size];
-        static constexpr uint32_t fmc_base_address = 0x08000000;
-        static constexpr uint32_t bank0_size = 512 * 1024;
-        static constexpr uint32_t bank0_end = fmc_base_address + bank0_size - 1;
-        static constexpr uint32_t fmc_end_address = fmc_base_address + _fmc_end;
-        static constexpr uint32_t data_area_start = fmc_end_address - 1024;
+private:
+    uint8_t buffer_[_storage_size];
+    static constexpr uint32_t fmc_base_address = 0x08000000;
+    static constexpr uint32_t fmc_end_address = fmc_base_address + _fmc_end;
+    static constexpr uint32_t data_area_start = fmc_end_address - 1024;
+    static uint8_t *offset = NULL;
 
-    public:
-        void begin()
+public:
+    void *begin()
+    {
+
+        uint32_t cnt;
+        fmc_unlock();
+
+        do
         {
-            fmc_unlock();
-
-            uint8_t *src = (uint8_t *)data_area_start;
-            for (uint32_t i = 0; i < _storage_size; i++) {
+            src = (uint8_t *)data_area_start + offset;
+            cnt = 0;
+            for (uint32_t i = 0; i < _storage_size; i++)
+            {
                 buffer_[i] = src[i];
+                if (src[i] == 0xff)
+                    cnt++;
             }
-        }
+        } while ((cnt == _storage_size) || (data_area_start + offset == fmc_end_address));
+    }
 
-        uint32_t length()
+    uint32_t length()
+    {
+        return _storage_size;
+    }
+
+    void read(uint32_t offset, uint8_t *data, uint32_t data_size)
+    {
+        // We always read from the buffer, as that's the most up-to-date. Flash is
+        // lagging behind until we commit(), but we may want to read before doing
+        // so.
+
+        // If we're out of bounds, or try to read too much, bail out.
+        if (offset > _storage_size || (data_size > (_storage_size - offset)))
+            return;
+
+        for (uint32_t i = 0; i < data_size; i++)
         {
-            return _storage_size;
+            data[i] = buffer_[i + offset];
         }
+    }
 
-        void read(uint32_t offset, uint8_t *data, uint32_t data_size)
+    void write(uint32_t offset, const uint8_t *data, uint32_t data_size)
+    {
+        // If we're out of bounds, or try to write too much, bail out.
+        if (offset > _storage_size || data_size > _storage_size)
+            return;
+
+        for (uint32_t i = 0; i < data_size; i++)
         {
-            // We always read from the buffer, as that's the most up-to-date. Flash is
-            // lagging behind until we commit(), but we may want to read before doing
-            // so.
-
-            // If we're out of bounds, or try to read too much, bail out.
-            if (offset > _storage_size || (data_size > (_storage_size - offset)))
-                return;
-
-            for (uint32_t i = 0; i < data_size; i++) {
-                data[i] = buffer_[i + offset];
-            }
+            buffer_[offset + i] = data[i];
         }
+    }
 
-        void write(uint32_t offset, const uint8_t *data, uint32_t data_size)
+    void commit()
+    {
+        uint32_t address = data_area_start;
+        uint32_t *ptrs = (uint32_t *)buffer_;
+        fmc_page_erase(address);
+        uint32_t word_count = _storage_size / 4;
+        uint32_t i = 0;
+        do
         {
-            // If we're out of bounds, or try to write too much, bail out.
-            if (offset > _storage_size || data_size > _storage_size)
-                return;
-
-            for (uint32_t i = 0; i < data_size; i++) {
-                buffer_[offset + i] = data[i];
-            }
-        }
-
-        void commit()
-        {
-            uint32_t address = data_area_start;
-            uint32_t *ptrs = (uint32_t *)buffer_;
-                fmc_page_erase(address);
-                uint32_t word_count = _storage_size / 4;
-                uint32_t i = 0;
-                do {
-                    fmc_word_program(address, *ptrs++);
-                    address += 4U;
-                } while (++i < word_count);
-        }
+            fmc_word_program(address, *ptrs++);
+            address += 4U;
+        } while (++i < word_count);
+    }
 };
